331c2e9652834e8ab3c3bbc401df3893c424ecf9
Yahia Helal <yahiarashwan348@gmail.com>
2023-12-20 02:04:00 +0200

    updated overall structure

--- games/mazeSolver/maze.py
+++ games/mazeSolver/maze.py
@@ -1,14 +1,45 @@
 import random
-from pyamaze import maze,agent
+from pyamaze import maze, agent , textLabel
 
-mazeSize = (20,20)
-xGoal,yGoal = random.randint(0 , mazeSize[0]) ,random.randint(0 , mazeSize[1])
-testMaze = maze(mazeSize[0],mazeSize[1])
-testMaze.CreateMaze(xGoal , yGoal)
+from games.miniGame import MiniGame
 
-xStrat , yStart = random.randint(0,testMaze.rows) , random.randint(0 , testMaze.cols)
 
-testAgent = agent(testMaze , xStrat , yStart , shape='arrow' ,color='red' , goal=(xGoal,yGoal))
+class Maze(MiniGame):
+
+    def __init__(self, title, mazeSize=(15, 20) , algorithmType="Default"):
+        super().__init__(title)
+        self.mazeSize = mazeSize
+        self.algorithmType = algorithmType
+        self.testMaze = maze(self.mazeSize[0], self.mazeSize[1])
+        self.xStartPosition = random.randint(0, self.testMaze.rows)
+        self.yStartPosition = random.randint(0, self.testMaze.cols)
+        self.xGoalPosition = random.randint(0, self.testMaze.rows)
+        self.yGoalPosition = random.randint(0, self.testMaze.cols)
+
+    def getTitle(self):
+        return self.title
+
+    def getMazeMap(self):
+        return self.testMaze.maze_map
+
+    def roadToGoal(self):
+        return self.testMaze.path
+
+    def traceGoal(self , agent , path):
+        self.testMaze.tracePath({agent : path} ,delay=120)
+
+    def startGame(self):
+        helper = Maze(self.getTitle())
+
+        self.testMaze.CreateMaze()
+        self.testAgent = agent(self.testMaze, self.xStartPosition, self.yStartPosition, shape='square', color='red',
+                               goal=(self.xGoalPosition, self.yGoalPosition), footprints=True, filled=True)
+        #self.cellsLabel = textLabel(self.testMaze, 'Total Cells', self.testMaze.rows * self.testMaze.cols)
+        self.AlgorithmLabel = textLabel(self.testMaze, 'Algorithm used' , self.algorithmType)
+        self.traceGoal(self.testAgent , self.testMaze.path)
+        self.testMaze.run()
+
+
 
 if "__main__" == __name__:
-    testMaze.run()
+    sampleMaze = Maze("Maze Solver")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import MainMenu
 from games.tictactoe import tictactoe
 from games.connect4 import connect4
+from games.mazeSolver import maze
 
 menu = MainMenu.MainMenu()
 
@@ -9,4 +10,5 @@ menu.setTitle("Mini Games")
 menu.addGame(tictactoe.TicTacToe("Tic Tac Toe"))
 # menu.addGame(None)
 menu.addGame(connect4.ConnectFour("Connect Four"))
+menu.addGame(maze.Maze("Maze Solver"))
 menu.startMenuLoop()

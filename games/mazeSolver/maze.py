25fb95a55a0ed5a74761cec4f19e2ea670a7125c
Yahia Helal <yahiarashwan348@gmail.com>
2023-12-21 07:38:37 +0200

    update maze randomness & solved multiple runs issue

--- games/mazeSolver/maze.py
+++ games/mazeSolver/maze.py
@@ -1,20 +1,25 @@
 import random
-from pyamaze import maze, agent , textLabel
+
+from pyamaze import maze, agent, textLabel, COLOR
 
 from games.miniGame import MiniGame
 
 
 class Maze(MiniGame):
+    MAZE_LINES = ['h', 'v', None]
 
-    def __init__(self, title, mazeSize=(15, 20) , algorithmType="Default"):
+    def __init__(self, title, mazeSize=(20,20), algorithmType="Default"):
         super().__init__(title)
+
         self.mazeSize = mazeSize
         self.algorithmType = algorithmType
-        self.testMaze = maze(self.mazeSize[0], self.mazeSize[1])
-        self.xStartPosition = random.randint(0, self.testMaze.rows)
-        self.yStartPosition = random.randint(0, self.testMaze.cols)
-        self.xGoalPosition = random.randint(0, self.testMaze.rows)
-        self.yGoalPosition = random.randint(0, self.testMaze.cols)
+
+        self.cellsLabel = None
+        self.destination = None
+        self.startPoint = None
+        self.testMaze = None
+        self.AlgorithmLabel = None
+        self.testAgent = None
 
     def getTitle(self):
         return self.title
@@ -25,20 +30,41 @@ class Maze(MiniGame):
     def roadToGoal(self):
         return self.testMaze.path
 
-    def traceGoal(self , agent , path):
-        self.testMaze.tracePath({agent : path} ,delay=120)
+    def __traceGoal(self, agent, path, maze):
+        maze.tracePath({agent: path}, delay=100)
+
+    def validatePath(self, start, goal):
+        while start == goal:
+            start = (random.randint(1, self.testMaze.rows), random.randint(1, self.testMaze.cols))
+            goal = (random.randint(1, self.testMaze.rows), random.randint(1, self.testMaze.cols))
+        return start, goal
 
     def startGame(self):
-        # helper = Maze(self.getTitle())
+        Maze(self.getTitle())
+        self.testMaze = maze(self.mazeSize[0], self.mazeSize[1])
 
-        self.testMaze.CreateMaze()
-        self.testAgent = agent(self.testMaze, self.xStartPosition, self.yStartPosition, shape='square', color='red',
-                               goal=(self.xGoalPosition, self.yGoalPosition), footprints=True, filled=True)
-        #self.cellsLabel = textLabel(self.testMaze, 'Total Cells', self.testMaze.rows * self.testMaze.cols)
-        self.AlgorithmLabel = textLabel(self.testMaze, 'Algorithm used' , self.algorithmType)
-        self.traceGoal(self.testAgent , self.testMaze.path)
+        self.startPoint, self.destination = self.validatePath(
+            (
+                random.randint(1, self.testMaze.rows),
+                random.randint(1, self.testMaze.cols)
+            ),
+
+            (
+                random.randint(1, self.testMaze.rows),
+                random.randint(1, self.testMaze.cols)
+            )
+        )
+
+        self.testMaze.CreateMaze(self.destination[0], self.destination[1], pattern=random.choice(Maze.MAZE_LINES))
+        self.testAgent = agent(self.testMaze, self.startPoint[0], self.startPoint[1], shape='square',
+                               color=COLOR.red,
+                               goal=self.destination, footprints=True, filled=True)
+
+        self.cellsLabel = textLabel(self.testMaze, 'Total Cells', self.testMaze.rows * self.testMaze.cols)
+        self.AlgorithmLabel = textLabel(self.testMaze, 'Algorithm used', self.algorithmType)
+
+        self.__traceGoal(self.testAgent, self.testMaze.path, self.testMaze)
         self.testMaze.run()
-
 
 
 if "__main__" == __name__:
